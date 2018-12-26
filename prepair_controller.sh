0fd96a67b6471d94d262ae532bd415b93508bc3b
mohamedgalal99 <mohamedgalal77@gmail.com>
2018-12-26 17:31:51 +0200

    add initial steps to configuration
    add initial config steps for controller and other nodes, before start install openstack

--- prepair_controller.sh
+++ prepair_controller.sh
@@ -4,3 +4,17 @@ apt update && apt upgrade -y
 apt install -y software-properties-common chrony vim
 apt-add-repository cloud-archive:liberty
 apt update
+
+# set static network config
+# In vim :set ai   => to make algin lines
+
+hostnamectl set-hostname cintroller
+rm rf /etc/ssh/ssh_host*
+dpkg-reconfigure openssh-server
+ssk-keygen
+
+apt install chrony
+chronyc source     # get from where we read NTP spurce
+vim /etc/chrony/chrony.conf #on other nodes, not controller
+   # server controller iburst
+systemctl restart chrony 
